9eecacb8b09a275167d11ec9f4ea4513ebb2a64c
Cathryn Ngaosi <cathrynngaosi@gmail.com>
2023-07-11 19:16:57 +0800

    Add routes and navlinks for AppNav

--- src/App.jsx
+++ src/App.jsx
@@ -10,10 +10,15 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Homepage />} />
+        <Route index element={<Homepage />} />
         <Route path="product" element={<Product />} />
         <Route path="pricing" element={<Pricing />} />
-        <Route path="app" element={<AppLayout />} />
+        <Route path="app" element={<AppLayout />}>
+          <Route index element={<p> List</p>} />
+          <Route path="cities" element={<p>LIst of cities </p>} />
+          <Route path="countries" element={<p>LIst of countries </p>} />
+          <Route path="form" element={<p>Form </p>} />
+        </Route>
         <Route path="login" element={<Login />} />
         <Route path="*" element={<PageNotFound />} />
       </Routes>
