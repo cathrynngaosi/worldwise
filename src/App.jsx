8d3c59c684d7e6310876da13cb372be2aa7f8af5
Cathryn Ngaosi <cathrynngaosi@gmail.com>
2023-07-12 11:05:50 +0800

    Setup dynamic route for City view

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import PageNotFound from "./pages/PageNotFound";
 import AppLayout from "./pages/AppLayout";
 import Login from "./pages/Login";
 import CityList from "./components/CityList";
+import City from "./components/City";
 import CountryList from "./components/CountryList";
 
 const URL = "http://localhost:8000";
@@ -46,6 +47,7 @@ function App() {
             path="cities"
             element={<CityList cities={cities} isLoading={isLoading} />}
           />
+          <Route path="cities/:id" element={<City />} />
           <Route
             path="countries"
             element={<CountryList cities={cities} isLoading={isLoading} />}
