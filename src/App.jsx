0e4d9d08ef48a6b25c56783cf0d99d74920a1c4d
Cathryn Ngaosi <cathrynngaosi@gmail.com>
2023-07-12 12:55:35 +0800

    Add Programmatic Navigation with useNavigate

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import Login from "./pages/Login";
 import CityList from "./components/CityList";
 import City from "./components/City";
 import CountryList from "./components/CountryList";
+import Form from "./components/Form";
 
 const URL = "http://localhost:8000";
 function App() {
@@ -52,7 +53,7 @@ function App() {
             path="countries"
             element={<CountryList cities={cities} isLoading={isLoading} />}
           />
-          <Route path="form" element={<p>Form </p>} />
+          <Route path="form" element={<Form />} />
         </Route>
         <Route path="login" element={<Login />} />
         <Route path="*" element={<PageNotFound />} />
